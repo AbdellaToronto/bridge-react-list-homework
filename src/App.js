70d5688e59e62d5007f752b13511327e17cef44e
Elizabeth Chan <elizabeth.chan@tulip.io>
2017-06-23 00:14:35 -0400

    Added the friends information.

--- src/App.js
+++ src/App.js
@@ -22,13 +22,31 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-
         {/*Make a list of all your friends*/}
-
+          <h1>My Friends</h1>
+          <ul>
+              {allFriends.map((friend, index) =>
+                  <li key={index}>
+                      {`${friend.first} ${friend.last}`}
+                  </li>
+              )}
+          </ul>
         {/*Make a list of just your best friends*/}
-
+          <h1>My Best Friends</h1>
+          <ul>
+              {allFriends.filter(friend =>
+                  friend.isBestFriend).map((friend, index) =>
+                  <li key={index}>
+                      {`${friend.first} ${friend.last}`}
+                  </li>
+              )}
+          </ul>
         {/*Show the average age of your best friends*/}
-
+          <h1>Friends' Average Age</h1>
+          <ul>
+              {allFriends.reduce((acc, friend) =>
+              acc + friend.age, 0)/allFriends.length}
+          </ul>
       </div>
     );
   }
