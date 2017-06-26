906f9c631f524d431ac01a31012f103a3da93da7
desaree <srhboo@gmail.com>
2017-06-26 13:18:01 -0400

    homework lists

--- src/App.js
+++ src/App.js
@@ -14,21 +14,31 @@ chance.mixin({
   })
 });
 
-const allFriends = Array.from({length: 20}, () => chance.friend());
+/* not sure the best place to define the following? 
+defined these here for the purpose of avoiding repetitive html in render section */
 
-console.log(allFriends);
+/* arrays of friend objects */
+const allFriends = Array.from({length: 20}, () => chance.friend());
+const bestFriends = allFriends.filter(friend => friend.isBestFriend);
+
+/* function: [friend] => [element object] */
+const listFriends = (friends) => friends.map(friend => <li className="friend">{`${friend.first} ${friend.last}`}</li>);
+
+/* arrays of element objects */
+const listAllFriends = listFriends(allFriends);
+const listBestFriends = listFriends(bestFriends);
+const averageAgeBestFriends = (bestFriends.reduce((acc, curr, i, arr) => acc + curr.age / arr.length, 0)).toFixed(2);
+
+/* array of headings with corresponding element object array */
+const listAndHeadings = [["all my friends",listAllFriends],["my best friends",listBestFriends],["avg age",averageAgeBestFriends]];
 
 export default class App extends Component {
   render() {
     return (
       <div className="App">
-
-        {/*Make a list of all your friends*/}
-
-        {/*Make a list of just your best friends*/}
-
-        {/*Show the average age of your best friends*/}
-
+        { listAndHeadings.map((list) => 
+          { return <div className="box"><h2>{list[0]}</h2><div className="list-wrapper"><ul>{list[1]}</ul></div></div> }
+        )}
       </div>
     );
   }
