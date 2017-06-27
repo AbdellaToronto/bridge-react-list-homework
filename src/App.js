8f9fdc0bc075ec73da9c733b4b1e2c094a134a7b
cgolinski <cgolinski@users.noreply.github.com>
2017-06-27 12:42:54 -0400

    refactored, styled

--- src/App.js
+++ src/App.js
@@ -16,65 +16,35 @@ chance.mixin({
 
 const allFriends = Array.from({length: 20}, () => chance.friend());
 
-console.log(allFriends);
+console.log(`allFriends: ${allFriends}`);
+
+const bestFriends = allFriends.filter(friend => friend.isBestFriend);
+
+const calculateAverageAge = (friends) => {
+  const sumOfFriendAges = friends.reduce((acc, val) => acc + val.age, 0);
+  return sumOfFriendAges / friends.length;
+};
 
 export default class App extends Component {
-  renderFriends(friends) {
-    return (
-      <div>
-        <h3>My friends: </h3>
-        {friends.map((friend) => <p>{`${friend.first} ${friend.last}`}</p>)}
-      </div>
-    );
-  }
-
-  renderBestFriends(friends) {
-    return (
-      <div>
-        <h3>My best friends: </h3>
-        {
-          friends.map((friend) => {
-            return friend.isBestFriend 
-              ? <p>{`${friend.first} ${friend.last}`}</p> 
-              : null;
-          })
-        }
-      </div>
-    );
-  }
-
-  renderAvgAgeOfBestFriends(friends) {
-    return (
-      <div>
-        <h3>The average age of my best friends: </h3>
-        {
-          friends.reduce((acc, val) => val.isBestFriend 
-            ? acc + val.age 
-            : acc
-            , 0
-          ) / 
-          friends.reduce((acc, val) => val.isBestFriend 
-            ? acc + 1 
-            : acc
-            , 0
-          )
-        }
-      </div>
-    );
-  }
-
   render() {
     return (
       <div className="App">
-        {/*Make a list of all your friends*/}
-        {this.renderFriends(allFriends)}
+        <main className="textArea">
+          <h1>Game Over</h1>
+          <h2>Final Score</h2>
 
-        {/*Make a list of just your best friends*/}
-        {this.renderBestFriends(allFriends)}
+          {/*Make a list of all your friends*/}
+          <h3>My friends:</h3>
+          {allFriends.map((friend) => <p>{`${friend.first} ${friend.last}`}</p>)}
 
-        {/*Show the average age of your best friends*/}
-        {this.renderAvgAgeOfBestFriends(allFriends)}
+          {/*Make a list of just your best friends*/}
+          <h3>My best friends:</h3>
+          {bestFriends.map((friend) => <p>{`${friend.first} ${friend.last}`}</p>)}
 
+          {/*Show the average age of your best friends*/}
+          <h3>Average age of my best friends:</h3>
+          {calculateAverageAge(bestFriends)}
+        </main>
       </div>
     );
   }
