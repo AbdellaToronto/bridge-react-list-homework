9b3fc2ccad04f0f8e4efd2ead8e0c95e043a25c6
Abdella <abdella@rangle.io>
2017-06-27 20:03:52 -0400

    some of the destructuring related content i talked about in class

--- src/App.js
+++ src/App.js
@@ -16,14 +16,49 @@ chance.mixin({
 
 const allFriends = Array.from({length: 20}, () => chance.friend());
 
-console.log(allFriends);
+const FriendElements = ({ friends, bestFriend }) => <div> <h1>Here is my best friend {bestFriend}</h1> {friends.map(friend => <h2>{friend.first} {friend.last}</h2>)}</div>;
+
+const myObject = {foo: 'hello', bar: 'world', foobar: 'Nick is also really cool'};
+const {bar, foo} = myObject;
+
+myObject.foo = 'Nick is unsure';
+console.log(foo);
+
+const ChildComp = (props) => (
+  <div>
+    <h1>{props.foo}</h1>
+    <h2>{props.bar}</h2>
+  </div>
+);
+
+const ParentComp = ({favTA, ...restProps}) => (
+
+  <div>
+    {console.log(restProps)}
+    <h2>My Favourite TA: {favTA}</h2>
+    <ChildComp {...restProps} />
+  </div>
+);
+
 
 export default class App extends Component {
+
+
+  friendMaker() {
+    return <h4>friends go here</h4>;
+  }
+
   render() {
     return (
       <div className="App">
 
         {/*Make a list of all your friends*/}
+        {/*<h2>Hello all of my friends</h2>*/}
+
+        <ParentComp favTA="Johanna" {...myObject} />
+
+        {/*<FriendElements friends={allFriends} bestFriend="Della" />*/}
+
 
         {/*Make a list of just your best friends*/}
 
